df969d8592d2be397d847c55d900c8c85d307634
amina.turd <aminaturdalieva@gmail.com>
2022-10-05 23:30:11 -0400

    added logic to change color of key when pressed

--- InteractivePiano/InteractivePiano.cs
+++ InteractivePiano/InteractivePiano.cs
@@ -2,43 +2,78 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using PianoSimulation;
+using System.Collections.Generic;
 using System;
 
 namespace InteractivePiano
 {
     public class InteractivePiano : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        public GraphicsDeviceManager graphics;
+        public SpriteBatch spriteBatch;
 
-        private Texture2D texture;
+        public Texture2D texture;
         private Texture2D texture2;
 
+        List<KeysPiano> whiteKeys;
+
+        List<KeysPiano> blackKeys;
 
         public InteractivePiano()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
         }
 
         protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+        {        
+            graphics.PreferredBackBufferWidth = 1540; 
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.ApplyChanges();
 
-            // Piano piano = new Piano();
-            // Audio audio = new Audio.Instance();
+            int whitePosX=0;
+            int whitePosY=0;
+            int whiteSizeX=70;
+            int whiteSizeY=220;
+            Color colorWhite=Color.White;
+            String[] letters = {"A", "B", "C", "D", "E", "F", "G"};
+            whiteKeys = new List<KeysPiano>();
+            for (int i=0; i<22; i++)
+            {
+                KeysPiano keysPiano = new KeysPiano(this, whitePosX, whitePosY, whiteSizeX, whiteSizeY, colorWhite, letters[i%letters.Length]);
+                whiteKeys.Add(keysPiano);
+                Components.Add(keysPiano);
+                whitePosX = whitePosX + 70;
+            }
+            int blackPosX=50;
+            int blackPosY=0;
+            int blackSizeX=40;
+            int blackSizeY=120;
+            Color colorBlack=Color.Black;
+            String[] lettersSharp = {"A#", "B#", "C#", "D#", "E#", "F#", "G#"};
+            blackKeys = new List<KeysPiano>(); 
+            for (int j=0; j<15; j++){
+                KeysPiano keysPiano = new KeysPiano(this, blackPosX, blackPosY, blackSizeX, blackSizeY, colorBlack,lettersSharp[j%lettersSharp.Length]);
+                Components.Add(keysPiano);
+                if (j==0 || j==2 || j== 5 || j==7 || j==10 || j==12)
+                {
+                    blackPosX = blackPosX + 140;
+                }
+                else {
+                    blackPosX = blackPosX + 70;
+                }
 
-
+            }       
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
             texture = Content.Load<Texture2D>("whiteRectangle");
             texture2 = Content.Load<Texture2D>("blackKey");
-            // TODO: use this.Content to load your game content here
 
         }
 
@@ -57,6 +92,19 @@ namespace InteractivePiano
             {
                 Piano piano = new Piano();
                 char pianoKey = key.ToString().ToLower()[0];
+
+                // if piano frequency is modulo of 110, call A
+                int index = piano.Keys.IndexOf(pianoKey);
+                if (piano.Wires[index].NoteFrequency % 110 == 0){
+                    foreach(KeysPiano whiteKey in whiteKeys)
+                    {
+                        if(whiteKey.letter=="A"){
+                            whiteKey.color = Color.Gray ;
+                        }
+
+                    }
+                }
+
                 System.Diagnostics.Debug.WriteLine(pianoKey);
                 piano.StrikeKey(pianoKey); 
                 // using (Audio audio = Audio.Instance){
@@ -78,58 +126,7 @@ namespace InteractivePiano
 
         protected override void Draw(GameTime gameTime)
         {
-
-            _graphics.PreferredBackBufferWidth = 1540; 
-            _graphics.PreferredBackBufferHeight = 500;
-            _graphics.ApplyChanges();
-
             GraphicsDevice.Clear(Color.Orange);
-
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(texture, new Rectangle(0, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(70, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(140, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(210, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(280, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(350, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(420, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(490, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(560, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(630, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(700, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(770, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(840, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(910, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(980, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1050, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1120, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1190, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1190, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1260, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1330, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1400, 0, 70, 220), Color.White);
-            _spriteBatch.Draw(texture, new Rectangle(1470, 0, 70, 220), Color.White);
-
-            _spriteBatch.Draw(texture2, new Rectangle(50, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(190, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(260, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(400, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(470, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(540, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(680, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(750, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(890, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(960, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(1030, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(1170, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(1240, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(1380, 0, 40, 120), Color.Black);
-            _spriteBatch.Draw(texture2, new Rectangle(1450, 0, 40, 120), Color.Black);
-
-
-
-            
-            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ InteractivePiano/Keys.cs
@@ -0,0 +1,55 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PianoSimulation;
+using System;
+
+namespace InteractivePiano
+{
+    public class KeysPiano : DrawableGameComponent
+    {
+        private InteractivePiano InteractivePiano;
+        private int posX;
+        private int posY;
+
+        private int sizeX;
+        private int sizeY;
+
+        public Color color{get; set;}
+
+        public String letter;
+
+        public KeysPiano(InteractivePiano piano, int posX, int posY, int sizeX, int sizeY, Color color, String letter): base(piano)
+        {
+           this.InteractivePiano = piano;
+           this.posX = posX;
+           this.posY = posY;
+           this.sizeX = sizeX;
+           this.sizeY = sizeY;
+           this.color = color;
+           this.letter = letter;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            InteractivePiano.spriteBatch.Begin();
+            InteractivePiano.spriteBatch.Draw(InteractivePiano.texture, new Rectangle(posX, posY, sizeX, sizeY), color);
+            InteractivePiano.spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
