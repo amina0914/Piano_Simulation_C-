4d5aad52587cdc7c345db5a9a0166637e725963d
amina.turd <aminaturdalieva@gmail.com>
2022-10-08 16:59:06 -0400

    added more comments

--- InteractivePiano/Program.cs
+++ InteractivePiano/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/**
+@author: Amina Turdalieva 
+@student id: 2035572
+@date: 08-10-2022
+@description: This the main class that runs the program. 
+It created an audio instance and insures that the audio is properly disposed at the end.
+*/
+
+using System;
 using PianoSimulation;
 
 namespace InteractivePiano
